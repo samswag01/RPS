b28310a40b2d92b17ecf370f885b7563a0026bda
samswag01 <stnlrssl@gmail.com>
2023-05-06 23:22:11 +0100

    Initial simple implementation

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rock Paper Scissors
+
+## Live Demonstration of the Clone
+[Live App](https://samswag01.github.io/RPS)
+
+- **if you see something that can be improved or bug in it, don't forget to reach out. Thanks.**
---
+++ script.js
@@ -0,0 +1,73 @@
+'use strict';
+
+let buttons = document.querySelectorAll('.play button'),
+    rock = document.querySelector('.rock'),
+    paper = document.querySelector('.paper'),
+    scissors = document.querySelector('.scissors'),
+    aiScoreBoard = document.querySelector('.ai span'),
+    playerScoreBoard = document.querySelector('.player span'),
+    aiScore = 0,
+    playerScore = 0,
+    aiName = 'AI',
+    gameCount = 0,
+    noIter = 5;
+
+const random = (arr) => {
+    return arr[Math.floor(Math.random() * 3)];
+}
+
+const playMove = (player, ai) => {
+    // const format = (winner) => `${winner} WINS!!`
+    if (player.move === ai.move) {
+        return 'TIE'
+    }
+
+    if (
+        (player.move === 'rock' && ai.move === 'scissors') ||
+        (player.move === 'scissors' && ai.move === 'paper') ||
+        (player.move === 'paper' && ai.move === 'rock')
+    ) return player.name;
+
+    return ai.name;
+}
+
+const updateScoreline = (winner) => {
+    if (winner === 'TIE') return;
+    if (winner === aiName) {
+        aiScore++;
+        aiScoreBoard.textContent = aiScore;
+    } else {
+        playerScore++;
+        playerScoreBoard.textContent = playerScore;
+    }
+}
+
+const gameOver = () => {
+    alert('GAME OVER!');
+    aiScore = 0;
+    playerScore = 0;
+    gameCount = 0;
+    aiScoreBoard.textContent = 0;
+    playerScoreBoard.textContent = 0;
+}
+
+const getPlayerMove = (event) => {
+    let playerMove = event.target.dataset.name;
+    let aiMove = random(['rock', 'paper', 'scissors']);
+    let winner = playMove({ name: playerName, move: playerMove }, { name: aiName, move: aiMove });
+    updateScoreline(winner);
+    winner = (winner === 'AI' || winner === playerName) ? `${winner} WINS!!` : `${winner}!!`;
+    alert(`${winner} => ${playerMove} vs ${aiMove}`);
+
+    // end game after noIter game has been played
+    gameCount++;
+    if (gameCount === noIter ) {
+        // a little delay before game actually ends.
+        setTimeout(gameOver, 1000);
+    }
+}
+
+buttons.forEach((button, _) => {
+    button.addEventListener('click', getPlayerMove);
+});
+
